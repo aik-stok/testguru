318b191e6884e4620292da4d036867de709243e6
aik-stok <aikb2b@gmail.com>
2018-02-13 15:14:29 +0300

    refactoring

--- app/helpers/assignment_helper.rb
+++ app/helpers/assignment_helper.rb
@@ -1,2 +1,24 @@
 module AssignmentHelper
+
+  def result_message_class(assignment)
+    if assignment.success?
+      "success"
+    else
+      "fail"
+    end
+  end
+
+  def result_message(assignment)
+    if assignment.success?
+      "Success"
+    else
+      "Test failed"
+    end
+  end
+
+  def question_number
+    "#{@assignment.test.questions.index(@assignment.current_question) + 1} /
+     #{@assignment.test.questions.count}"
+  end
+    
 end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,11 +4,11 @@ class Assignment < ApplicationRecord
   belongs_to :current_question, class_name: "Question", optional: true
   
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def accept!(answer_ids)
-    self.correct_questions += 1 if correct_answer?(answer_ids)
+#    self.correct_questions += 1 if correct_answer?(answer_ids)
     self.correct_answers_sum += correct_answers.where(id: answer_ids).count
-    self.current_question = next_question
     save!
   end 
 
@@ -17,7 +17,11 @@ class Assignment < ApplicationRecord
   end
 
   def percent_of
-    (correct_answers_sum.to_f / all_correct_answers.to_f * 100.0).to_i
+    (correct_answers_sum / all_correct_answers.to_f * 100.0).round
+  end
+  
+  def success?
+    percent_of > 85
   end
 
   private
@@ -25,7 +29,11 @@ class Assignment < ApplicationRecord
   def before_validation_set_first_question
     self.current_question = test.questions.first if current_question.nil?
   end
-  
+
+  def before_validation_set_next_question
+    self.current_question = next_question
+  end
+
   def all_correct_answers
     Answer.correct.includes(:question).where(questions: {test_id: test.id}).count
   end
@@ -34,12 +42,13 @@ class Assignment < ApplicationRecord
     current_question.answers.correct
   end
   
-  def correct_answer?(answer_ids)
-    correct_answers.count == correct_answers.where(id: answer_ids).count && correct_answers.count == answer_ids.count
-  end
+#  def correct_answer?(answer_ids)
+#    correct_answers.count == correct_answers.where(id: answer_ids).count && correct_answers.count == answer_ids.count
+#  end
   
   def next_question
     test.questions.order(:id).where("id > ?", current_question.id).first
   end
+
 
 end
