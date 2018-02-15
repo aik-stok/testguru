82c014ee534977fabb354d7cbc0b4c562ecef515
aik-stok <aikb2b@gmail.com>
2018-02-15 12:56:51 +0300

    message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,7 @@ module ApplicationHelper
     link_to "#{repo}", "https://github.com/#{author}/#{repo}", target: :_blank
   end
 
-
+  def flash_message
+    content_tag :p, flash[:alert], class: 'flash alert' if flash[:alert]
+  end
 end
