b25c327391371981e99bed150bf404348c1b32f3
aik-stok <aikb2b@gmail.com>
2018-03-16 00:48:36 +0300

    inline updater error fix

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -7,7 +7,7 @@ document.addEventListener('turbolinks:load', function() {
   }
 
   var errors = document.querySelector('.resource-errors')
-if (errors) {
+if (controls.length && errors) {
 	var resourceId = errors.dataset.resourceId
 	formInlineHandler(resourceId)
 }
