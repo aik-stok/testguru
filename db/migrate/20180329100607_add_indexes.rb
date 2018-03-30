385e2eb80528591b1a54741aaf03d6dca8595943
aik-stok <aikb2b@gmail.com>
2018-03-30 13:13:28 +0300

    heroku

--- db/migrate/20180329100607_add_indexes.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexes < ActiveRecord::Migration[5.1]
-  def change
-    add_index :gists,  :url
-    add_index :tests, %i[title level]
-    add_index :users, %i[first_name last_name]
-  end
-end
