00e8ca4c6e8178424617e35214fcaa94fcb09e7b
aik-stok <aikb2b@gmail.com>
2018-04-04 10:18:18 +0300

    smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "testguru_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {host: 'aik-testguru.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_setings = {
+    address:        'smtp.gmail.com',
+    port:           587,
+    user_name:      ENV['SMTP_USERNAME'],
+    password:       ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_start_tls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
