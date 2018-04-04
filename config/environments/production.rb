dfee003310b5618bd839bff9076ab95a89dc0ff0
aik-stok <aikb2b@gmail.com>
2018-04-04 11:31:10 +0300

    smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {host: 'aik-testguru.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_setings = {
+  config.action_mailer.smtp_settings = {
     address:        'smtp.gmail.com',
     port:           587,
     user_name:      ENV['SMTP_USERNAME'],
